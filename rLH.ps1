d62b6c5cb9c3c0494845320c35bc0bac1db4a567
Oaker Min <7734956+Brucius@users.noreply.github.com>
2020-07-29 15:25:51 +0800

    Update rLH.ps1

--- rLH.ps1
+++ rLH.ps1
@@ -4,5 +4,5 @@ $Subscriptions = Get-AzureRmSubscription
 
 foreach ($sub in $Subscriptions) {
     Set-AzureRmcontext -Tenant $TenantId -SubscriptionId $sub
-    New-AzureRmDeployment -Name 'rhipeMSP' -Location 'Australia East' -TemplateFile 'dRM.json' -TemplateParameterFile 'dRMpara1.json' -Verbose
+    New-AzureRmDeployment -Name 'rhipeMSP' -Location 'Australia East' -TemplateFile 'dRM.json' -TemplateParameterFile 'dRMpara.json' -Verbose
 }
