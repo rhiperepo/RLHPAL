165c679c62ee5532710615687829a6284f855f3d
Oaker Min <7734956+Brucius@users.noreply.github.com>
2020-07-29 15:14:06 +0800

    Update rLH.ps1
    Updated to use RM module

--- rLH.ps1
+++ rLH.ps1
@@ -1,5 +1,8 @@
-$Subscriptions = Get-AzSubscription
-foreach ($sub in $Subscriptions){
-set-Azcontext -SubscriptionId $sub.Id
-New-AzDeployment -Name 'rhipeMSP' -Location 'Australia East' -TemplateFile 'dRM.json' -TemplateParameterFile 'dRMpara.json' -Verbose
+Login-AzureRmAccount
+$tid = Get-AzureRmTenant
+$Subscriptions = Get-AzureRmSubscription
+
+foreach ($sub in $Subscriptions) {
+    Set-AzureRmcontext -Tenant $tid -SubscriptionId $sub.I
+    New-AzureRmDeployment -Name 'rhipeMSP' -Location 'Australia East' -TemplateFile 'dRM.json' -TemplateParameterFile 'dRMpara1.json' -Verbose
 }
