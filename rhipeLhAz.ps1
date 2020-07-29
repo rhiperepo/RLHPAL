a7009631e469d8386b7d43a3923d1ab0ef71b0a2
Oaker Min <7734956+Brucius@users.noreply.github.com>
2020-07-29 15:23:23 +0800

    Update rhipeLhAz.ps1

--- rhipeLhAz.ps1
+++ rhipeLhAz.ps1
@@ -3,6 +3,6 @@ $TenantId = Get-AzTenant
 $Subscriptions = Get-AzSubscription
 
 foreach ($sub in $Subscriptions) {
-    Set-Azcontext -Tenant $TenantId -SubscriptionId $sub.I
+    Set-Azcontext -Tenant $TenantId -SubscriptionId $sub
     New-AzDeployment -Name 'rhipeMSP' -Location 'Australia East' -TemplateFile 'dRM.json' -TemplateParameterFile 'dRMpara1.json' -Verbose
 }
